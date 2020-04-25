438183453843fb649646379028fbbd01afb293db
Douglas-Silverman <53188336+Douglas-Silverman@users.noreply.github.com>
2020-04-24 23:09:16 -0400

    changed port

--- server-main.js
+++ server-main.js
@@ -4,4 +4,4 @@ var mongo_database_1 = require("./mongo-database");
 var myserver_post_1 = require("./myserver-post");
 var theDatabase = new mongo_database_1.Database("kashsomani"); // CHANGE THIS
 var theServer = new myserver_post_1.MyServer(theDatabase);
-theServer.listen(process.env.PORT || 5000)
+theServer.listen(process.env.PORT || 5000);
